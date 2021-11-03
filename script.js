b13a432e9f5d73c0a8a554f497bcb3dbf5ba3cc8
zachary sibson <zac.sibson@gmail.com>
2021-11-03 17:24:51 -0600

    fixed some syntax errors in script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-/* Some bugs in this.
 
-const WHITE_KEYS = ['z', 'x' 'c', 'v', 'b', 'n', 'm']
+
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
-*/
+
 const keys = document.querySelectorAll('.key')
 
 keys.forEach(key => {
     key.addEventListener('click' , () => playNote(key))
 })
-/* Also bugs in this, not sure what I did.
 
-document.addEventListener('keydown' e => {
+
+document.addEventListener('keydown', e => {
     if (e.repeat) return
     const key = e.key
     const whiteKeyIndex = WHITE_KEYS.indexOf(key)
@@ -19,8 +19,9 @@ document.addEventListener('keydown' e => {
     if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
     if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
 })
-*/
+
 function playNote(key) {
+    console.log(key);
     const noteAudio = document.getElementById(key.dataset.note)
     noteAudio.currentTime = 0
     noteAudio.play()
